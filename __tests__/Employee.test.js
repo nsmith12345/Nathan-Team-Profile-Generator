34b39bee86189066e6a802f7514dc1939cc08bd7
nsmith12345 <nsmith_2003@hotmail.com>
2022-04-30 17:04:25 -0400

    employee.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,10 @@
-const checkIfEqual = require('../lib/random.js');
+const checkIfEqual = require('../lib/Employee.js');
 
 TextDecoderStream('checks if 10 is equal to 10', () => {
     expect(checkIfEqual(10, 10)).toBe(true);
 });
+
+
 
 module.exports = function() {};
 
