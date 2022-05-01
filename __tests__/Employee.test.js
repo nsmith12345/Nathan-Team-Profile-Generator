4e869a11543038a7524c618bf80b67f22fb3e768
nsmith12345 <nsmith_2003@hotmail.com>
2022-05-01 19:12:37 -0400

    employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,34 @@
-const checkIfEqual = require('../lib/Employee.js');
+const Employee = require('../lib/Employee.js');
 
-TextDecoderStream('checks if 10 is equal to 10', () => {
-    expect(checkIfEqual(10, 10)).toBe(true);
+test('creates a new employee object', () => {
+    const employee = new Employee('Nathan', 7, 'nsmith_2003@hotmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    
 });
 
+test('gets employee name', () => {
+    const employee = new Employee('Nathan', 7, 'nsmith_2003@hotmail.com');
 
+    expect(employee.getName()).toEqual(expect.any(String));
+});
 
-module.exports = function() {};
+test('gets employee id', () => {
+    const employee = new Employee('Nathan', 7, 'nsmith_2003@hotmail.com');
 
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('gets employee email', () => {
+    const employee = new Employee('Nathan', 7, 'nsmith_2003@hotmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+test('gets role of employee', () => {
+    const employee = new Employee('Nathan', 7, 'nsmith_2003@hotmail.com');
+
+    expect(employee.getRole()).toEqual("Employee");
+});
